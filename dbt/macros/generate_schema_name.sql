f292a560d32690bf739adad60efaf0fa1a39b9b9
Anton Stechman <stechman.ab@gmail.com>
2026-07-12 10:49:37 +1200

    fix macro to account for path-sep differences between local and cloud ops.
    i.e., "\\" vs "/"

--- dbt/macros/generate_schema_name.sql
+++ dbt/macros/generate_schema_name.sql
@@ -1,6 +1,6 @@
 {% macro generate_schema_name(custom_name, node) -%}
-    {%- set rel_path = node.original_file_path -%}
-    {%- set dirs = rel_path.split("\\") -%}
+    {%- set rel_path = node.original_file_path.replace("\\", "/") -%}
+    {%- set dirs = rel_path.split("/") -%}
     {%- if dirs|length >= 4 and dirs[1] == "sources" -%}
         {{ dirs[3] }}
     {%- elif dirs|length >= 3 and dirs[1] == "domains" -%}
